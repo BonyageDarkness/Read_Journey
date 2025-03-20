f04ff813254330693395fc5df2205595ea027efd
Bohdan <bosrok.ke@gmail.com>
2025-03-20 22:11:50 +0000

    Dashboard adaptive without filter redux

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,84 @@
+import styles from "./Dashboard.module.scss";
+import book1x from "../../img/dashboard/book.png";
+import book2x from "../../img/dashboard/book@2x.png";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
+export default function Dashboard() {
+  return (
+    <div className={styles.dashboardContainer}>
+      <form className={styles.filterForm} onSubmit={(e) => e.preventDefault()}>
+        <p className={styles.filterFormTitle}>Filters:</p>
+
+        <div className={styles.filterInputWrapper}>
+          <span className={styles.filterLabel}>Book title:</span>
+          <input
+            type="text"
+            placeholder="Enter text"
+            className={styles.filterInput}
+          />
+        </div>
+
+        <div className={styles.filterInputWrapper}>
+          <span className={styles.filterLabel}>The author:</span>
+          <input
+            type="text"
+            placeholder="Enter text"
+            className={styles.filterInput}
+          />
+        </div>
+
+        <button type="submit" className={styles.filterSubmit}>
+          To apply
+        </button>
+      </form>
+
+      <div className={styles.dashboardInfo}>
+        <p className={styles.dashboardInfoTitle}>Start your workout</p>
+
+        <ul className={styles.dashboardInfoList}>
+          <li className={styles.dashboardInfoItem}>
+            <div className={styles.numberCircle}>1</div>
+            <p className={styles.dashboardInfoText}>
+              <span className={styles.dashboardInfoItemTitle}>
+                Create a personal library:
+              </span>{" "}
+              add the books you intend to read to it.
+            </p>
+          </li>
+          <li className={styles.dashboardInfoItem}>
+            <div className={styles.numberCircle}>2</div>
+            <p className={styles.dashboardInfoText}>
+              <span className={styles.dashboardInfoItemTitle}>
+                Create your first workout:
+              </span>{" "}
+              define a goal, choose a period, start training.
+            </p>
+          </li>
+        </ul>
+        <div className={styles.dashboardLink}>
+          <p className={styles.switch}>
+            <a href="/library">My library</a>
+          </p>
+          <a href="/library" className={styles.switchIconLink}>
+            <svg className={styles.switchIcon}>
+              <use href="/sprite.svg#icon-log-in" />
+            </svg>
+          </a>
+        </div>
+      </div>
+      <div className={styles.dashboardBook}>
+        <img
+          className={styles.dashboardBookImg}
+          src={book1x}
+          srcSet={`${book1x} 1x, ${book2x} 2x`}
+          alt="book"
+        />{" "}
+        <p className={styles.dashboardBookText}>
+          "Books are <span className={styles.dashboardBookspan}>windows</span>{" "}
+          to the world, and reading is a journey into the unknown."
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/pages/Recommended/RecommendedPage.jsx
+++ src/pages/Recommended/RecommendedPage.jsx
@@ -1,9 +1,18 @@
+import { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import styles from "./RecommendedPage.module.scss";
+
 import Header from "../../components/Header/Header";
+import Dashboard from "../../components/Dashboard/Dashboard";
 
 export default function RecommendedPage() {
   return (
     <>
-      <Header />
+      <div className={styles.containerRecommended}>
+        <Header />
+        <Dashboard />
+        <Suspense fallback={<div>Загрузка...</div>}></Suspense>
+      </div>
     </>
   );
 }
